f9c323d036886fdb0e11e5914ea359c9ac1e78b0
daviiisousa <sousadavi248@gmail.com>
2025-05-07 21:34:15 -0300

    chore: update NotFound page layout and styles

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 export function PageNotFound(){
     return(
-        <h1>404 error</h1>
+        <div 
+            className="h-screen flex justify-center items-center flex-col gap-6"
+        >
+            <h1
+                className="text-center text-7xl text-primary  font-bold "
+            >
+                404 Error
+            </h1>
+            <Link to={"/"} className="underline hover:text-primary transition">
+                voltar para Login
+            </Link>
+        </div>
     )
 }
